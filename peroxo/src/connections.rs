bbb9b32df7210a5d5a00f47238a518002825c5f4
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2025-09-28 14:45:05 +0530

    feat: app_state builder pattern

--- peroxo/src/connections.rs
+++ peroxo/src/connections.rs
@@ -1,9 +1,9 @@
 use crate::actors::chat_service::chat_service_client::ChatServiceClient;
 use tonic::transport::Channel;
 
-pub async fn connect_chat_service_client()
--> Result<ChatServiceClient<Channel>, Box<dyn std::error::Error>> {
-    let chat_service_addr = std::env::var("CHAT_SERVICE_ADDR")?;
+pub async fn connect_chat_service_client(
+    chat_service_addr: String,
+) -> Result<ChatServiceClient<Channel>, Box<dyn std::error::Error>> {
     let client = ChatServiceClient::connect(chat_service_addr).await?;
     Ok(client)
 }
