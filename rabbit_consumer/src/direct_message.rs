3103031017a74eac82feb4f6f79a87ea3d3ea47c
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2025-10-08 23:27:36 +0530

    feat(rabbit_consumer): handle room_messages

--- rabbit_consumer/src/direct_message.rs
+++ rabbit_consumer/src/direct_message.rs
@@ -1,5 +1,3 @@
-use std::any::type_name;
-
 use scylla::{
     client::session::Session,
     statement::{
@@ -8,7 +6,10 @@ use scylla::{
     },
     value::CqlTimestamp,
 };
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+use crate::message_handler::MessageHandler;
 
 pub struct DirectMessage {
     pub conversation_id: String,
@@ -60,4 +61,57 @@ pub async fn write_direct_message(
     session.batch(&batch, batch_values).await?;
 
     Ok(())
+}
+
+pub struct DirectMessageHandler<'a> {
+    pub session: &'a Session,
+}
+
+#[async_trait::async_trait]
+impl<'a> MessageHandler for DirectMessageHandler<'a> {
+    async fn process_message(&self, data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+        let msg: SerializableDirectMessage = serde_json::from_slice(data)?;
+        let dm: DirectMessage = msg.into();
+
+        if let Err(e) = write_direct_message(self.session, dm).await {
+            tracing::error!("Failed to write room message for room_queue: {}", e);
+        }
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct SerializableDirectMessage {
+    pub conversation_id: String,
+    pub message_id: Uuid,
+    pub sender_id: i32,
+    pub recipient_id: i32,
+    pub message_text: String,
+    pub created_at: i64,
+}
+
+impl From<DirectMessage> for SerializableDirectMessage {
+    fn from(dm: DirectMessage) -> Self {
+        Self {
+            conversation_id: dm.conversation_id,
+            message_id: dm.message_id,
+            sender_id: dm.sender_id,
+            recipient_id: dm.recipient_id,
+            message_text: dm.message_text,
+            created_at: dm.created_at.0,
+        }
+    }
+}
+
+impl From<SerializableDirectMessage> for DirectMessage {
+    fn from(sdm: SerializableDirectMessage) -> Self {
+        Self {
+            conversation_id: sdm.conversation_id,
+            message_id: sdm.message_id,
+            sender_id: sdm.sender_id,
+            recipient_id: sdm.recipient_id,
+            message_text: sdm.message_text,
+            created_at: CqlTimestamp(sdm.created_at),
+        }
+    }
 }
